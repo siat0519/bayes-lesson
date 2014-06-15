29bb60a648c1a27a6678b833baa948a0ed37ba1d
Takahiro Kamatani <buhii314@gmail.com>
2014-06-15 19:14:13 +0900

    ハッシュにした

--- rain_a_b.rb
+++ rain_a_b.rb
@@ -5,56 +5,71 @@ end
 
 
 # -----
-sun, rain = 7, 1
-sun_b_drink_with_a, sun_b_doesnt_drink_with_a = 4, 1
-rain_b_drink_with_a, rain_b_doesnt_drink_with_a = 3, 2
+weather = {
+  :sun => 7,
+  :rain => 1
+}
+
+B_and_A = {
+  :sun => {
+    :yes => 4,
+    :no  => 1
+  },
+  :rain => {
+    :yes => 3,
+    :no  =>2
+  }
+}
 
 # -----
+# 確率:
+P = {}
+
 # 目標:
 # 「BさんがAさんの誘いに乗った時に
-#   雨が降っていない確率を求める」 ->  P(H_sun|D)
+#   雨が降っていない確率を求める」
+#  P(H_sun|D)  ->  P[:H_sun_D]
 
 # D(データ): BさんがAさんの誘いに乗った
 # H(仮定)  : 雨が降っていない
 # H_sun
 # H_rain
-
-# P(H_sun)
 puts "-" * 40
 puts "事前確率"
-p_h_sun = (sun) / (sun + rain).to_f
-puts "P(H_sun): %0.3f" % p_h_sun
+P["H_sun"] = (weather[:sun]) / (weather[:sun] + weather[:rain]).to_f
+puts "P(H_sun): %0.3f" % P["H_sun"]
 
 # P(H_rain)
-p_h_rain = (rain) / (sun + rain).to_f
-puts "P(H_rain): %0.3f" % p_h_rain
+P["H_rain"] = (weather[:rain]) / (weather[:sun] + weather[:rain]).to_f
+puts "P(H_rain): %0.3f" % P["H_rain"]
+
 
 puts "-" * 40
 puts "尤度"
+
+P["D|H_sun"] = B_and_A[:sun][:yes] / (B_and_A[:sun][:yes] + B_and_A[:sun][:no]).to_f
+puts "P(D|H_sun): %0.3f" % P["D|H_sun"]
+
+P["D|H_rain"] = B_and_A[:rain][:yes] / (B_and_A[:rain][:yes] + B_and_A[:rain][:no]).to_f
+puts "P(D|H_rain): %0.3f" % P["D|H_rain"]
+
 # P(D) = P(D|H_sun)P(H_sun) + P(D|H_rain)P(H_rain)
-p_d_h_sun = sun_b_drink_with_a / (sun_b_drink_with_a + sun_b_doesnt_drink_with_a).to_f
-puts "P(D|H_sun): %0.3f" % p_d_h_sun
-
-p_d_h_rain = rain_b_drink_with_a / (rain_b_drink_with_a + rain_b_doesnt_drink_with_a).to_f
-puts "P(D|H_rain): %0.3f" % p_d_h_rain
-
 puts "-" * 40
 puts "ベイズの展開公式"
-p_d = p_d_h_sun * p_h_sun + p_d_h_rain * p_h_rain
-puts "P(D) = P(D|H_sun) * P(H_sun) + P(D|H_rain) * P(H_rain): %0.3f" % p_d
+P["D"] = P["D|H_sun"] * P["H_sun"] + P["D|H_rain"] * P["H_rain"]
+puts "P(D) = P(D|H_sun) * P(H_sun) + P(D|H_rain) * P(H_rain): %0.3f" % P["D"]
 
 
 # 分子の式
 # P(D|Hi)P(Hi)
 # でも、晴れの時だから...
 # -> P(D|H_sun)P(H_sun)
-
 puts "-" * 40
 puts "ベイズの定理を適用（事後確率）"
 
 # ベイズの定理
 # 目標
 # P(H_sun|D) = P(D|H_sun)P(H_sun) / P(D)
-p_h_sun_d = p_d_h_sun * p_h_sun / p_d
-puts "P(H_sun|D): %0.3f" % p_h_sun_d
+P["H_sun|D"] = P["D|H_sun"] * P["H_sun"] / P["D"]
+puts "P(H_sun|D): %0.3f" % P["H_sun|D"]
 puts "-" * 40
